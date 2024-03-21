4707ee7eaa8d0bff8f4a606f5d9f5fb8215cc700
Bunzu013 <83347605+Bunzu013@users.noreply.github.com>
2024-03-21 22:25:41 +0100

    blocks landing on top of each other

--- tetris.py
+++ tetris.py
@@ -2,15 +2,27 @@ from settings import *
 from tetromino import Tetromino
 import math
 
+
 class Tetris:
     def __init__(self, app):
         self.app = app
         self.sprite_group = pg.sprite.Group()
+        self.field_array = self.get_field_array()
         self.tetromino = Tetromino(self)
 
+    def put_tetromino_blocks_in_array(self):
+        for block in self.tetromino.blocks:
+            x, y = int(block.pos.x), int(block.pos.y)
+            self.field_array[y][x] = block
+
+    def get_field_array(self):
+        return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
+
     def check_tetromino_landing(self):
-        if self.tetromino_landing:
+        if self.tetromino.landing:
+            self.put_tetromino_blocks_in_array()
             self.tetromino = Tetromino(self)
+
     def control(self, pressed_key):
         if pressed_key == pg.K_LEFT:
             self.tetromino.move(direction='left')
@@ -21,7 +33,8 @@ class Tetris:
         for x in range(FIELD_W):
             for y in range(FIELD_H):
                 pg.draw.rect(self.app.screen, 'black',
-                             (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE),1)
+                             (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+
     def update(self):
         if self.app.anim_trigger:
             self.tetromino.update()
--- tetromino.py
+++ tetromino.py
@@ -1,6 +1,7 @@
 from settings import *
 import random
 
+
 class Block(pg.sprite.Sprite):
 
     def __init__(self, tetromino, pos):
@@ -12,6 +13,7 @@ class Block(pg.sprite.Sprite):
         self.image.fill('orange')
 
         self.rect = self.image.get_rect()
+
     def set_rect_pos(self):
         self.rect.topleft = self.pos * TILE_SIZE
 
@@ -20,7 +22,8 @@ class Block(pg.sprite.Sprite):
 
     def is_collide(self, pos):
         x, y = int(pos.x), int(pos.y)
-        if 0 <= x < FIELD_W and y < FIELD_H:
+        if 0 <= x < FIELD_W and y < FIELD_H and (
+            y < 0 or not self.tetromino.tetris.field_array[y][x]):
             return False
         return True
 
@@ -31,8 +34,10 @@ class Tetromino:
         self.shape = random.choice(list(TETROMINOES.keys()))
         self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
         self.landing = False
+
     def is_collide(self, block_positions):
         return any(map(Block.is_collide, self.blocks, block_positions))
+
     def move(self, direction):
         move_direction = MOVE_DIRECTIONS[direction]
         new_block_positions = [block.pos + move_direction for block in self.blocks]
@@ -43,5 +48,6 @@ class Tetromino:
                 block.pos += move_direction
         elif direction == 'down':
             self.landing = True
+
     def update(self):
         self.move(direction='down')
